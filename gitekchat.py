59f9358228bb16c870f1ae8c49870e341bf764ca
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-19 00:50:46 +0100

    Update gitekchat.py

--- gitekchat.py
+++ gitekchat.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+with st.chat_message("assistant"):
+    st.write("Hello human")
+    
 prompt = st.chat_input("Say something")
 if prompt:
     st.write(f"User has sent the following prompt: {prompt}")
