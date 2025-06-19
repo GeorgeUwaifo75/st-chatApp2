c34460fd857b7c818d637745fad674fd1544d937
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2025-06-19 14:38:45 +0100

    Update gimodelgpt.py

--- gimodelgpt.py
+++ gimodelgpt.py
@@ -4,7 +4,7 @@ import os
 #def do_config():
 #   st.write("Hello from a function")
 
-if __name__ == '__main__':
-    st.title('My LLM Experiment')
+#if __name__ == '__main__':
+    #st.title('My LLM Experiment')
     # Execute when the module is not initialized from an import statement.
     #do_config()
