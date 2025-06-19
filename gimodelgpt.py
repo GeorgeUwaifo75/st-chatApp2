77fa10a46857838422b72242331ce78428597d9f
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2025-06-19 15:12:51 +0100

    Update gimodelgpt.py

--- gimodelgpt.py
+++ gimodelgpt.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import os
 
-#def do_config():
-#   st.write("Hello from a function")
+def do_config():
+   st.write("Hello from a function")
 
-#if __name__ == '__main__':
-    #st.title('My LLM Experiment')
+if __name__ == '__main__':
+    st.title('My LLM Experiment')
     # Execute when the module is not initialized from an import statement.
-    #do_config()
+    do_config()
